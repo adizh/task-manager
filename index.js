174301646a739cba7721718e91eded36473a0a20
adinaizhamangul <83157462+adizh@users.noreply.github.com>
2021-12-19 11:14:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/task');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+app.use(express.static('./public'))
+app.use(express.json())
+app.use('/api/v1/tasks', tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = 8080;
+const mySecret = process.env.DB
+const start = async () => {
+  try {
+    await connectDB(mySecret);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
